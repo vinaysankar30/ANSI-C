727799596b0676a1c962657cc5341a6481c3ca83
abspython <abhijithsheheer@gmail.com>
2019-04-07 21:39:13 +0530

    Correct 28.c

--- 28.c
+++ 28.c
@@ -4,23 +4,20 @@
 
 #include <stdio.h>
 #include <string.h>
-/*void ptrrev(int *ptr,int s){
-	char rev[10];
-	for(int i=s;i>0;i--){
-		rev[i] = *ptr;
-		ptr++;
-	}
-	printf("%s\n",rev);
-}*/
 void main(){
-	char str[10];
+	char str[20];
 	int s;
 	scanf("%s",str);
 	s = strlen(str);
-	char rev[10];
-	for (int i = s; i > 0; i--)
-	{
-		rev[i] = *str;
+	char *eptr,*sptr;
+	sptr = &str;
+	eptr = &str;
+	//char rev[s];
+	for(int i=0;i<s-1;i++,eptr++);
+	for(int i=0;i<s/2;i++,sptr++,eptr--){
+		char temp = *sptr;
+		*sptr = *eptr;
+		*eptr = temp;
 	}
-	printf("%s\n",rev);
+	printf("%s\n",str);
 }
